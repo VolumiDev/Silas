15c7e1dc20dd034784d2693c07dcc8a5f897a40c
VolumiDev <diego.martga@gmail.com>
2025-02-08 20:44:21 +0100

    terminado la carga en panel central

---
+++ Silas/Models/Offers/OffersResponseToStudentProfile.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Silas.Models.Offers
+{
+    public class OffersResponseToStudentProfile
+    {
+        [JsonPropertyName("status")]
+        public long Status { get; set; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("offers")]
+        public List<OffersResponseToStudentProfile> Offers { get; set; }
+    }
+}
