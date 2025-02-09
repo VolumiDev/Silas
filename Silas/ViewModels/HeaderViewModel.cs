558e5d33c13cd27b55c5475930a82706fcbb50c9
VolumiDev <diego.martga@gmail.com>
2025-02-09 11:48:33 +0100

    Añadimos viewModels

---
+++ Silas/ViewModels/HeaderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Silas.ViewModels
+{
+    public class HeaderViewModel
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
